eec59f0a64436403f32bb0b928302caece1e2e56
Aman Mishra <aman07mishra@gmail.com>
2023-08-04 22:48:55 +0530

    Add immediate food

---
+++ immediate_food.sql
@@ -0,0 +1,9 @@
+--Link - https://leetcode.com/problems/immediate-food-delivery-ii/
+Select 
+    round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
+from Delivery
+where (customer_id, order_date) in (
+  Select customer_id, min(order_date) 
+  from Delivery
+  group by customer_id
+);
