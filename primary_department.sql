b218c7098e1c2a1ce21d8148629db43e5357e672
Aman Mishra <aman07mishra@gmail.com>
2023-11-15 22:29:59 +0530

    add problems

---
+++ primary_department.sql
@@ -0,0 +1,9 @@
+-- Link - https://leetcode.com/problems/primary-department-for-each-employee/
+SELECT employee_id, department_id
+from employee
+where primary_flag = 'Y' or employee_id in (
+    select employee_id 
+    from employee
+    group by employee_id
+    having count(employee_id) = 1
+)
